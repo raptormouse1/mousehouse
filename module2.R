c8167627e4661836787767e770622813043fd02e
raptormouse1 <46422858+raptormouse1@users.noreply.github.com>
2019-01-17 23:19:59 -0500

    Add files via upload

---
+++ module2.R
@@ -0,0 +1,4 @@
+assignment2 <- c(16,18,14,22,27,17,19,17,17,22,20,22) 
+myMean <- function(assignment2){return(sum(assignment)/length(someData))}
+myMean(assignment2)
+#Error in myMean(assignment2) : object 'assignment' not found
