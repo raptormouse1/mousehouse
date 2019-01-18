14b34a897c87c6a027038feaad7985242b2311d0
raptormouse1 <46422858+raptormouse1@users.noreply.github.com>
2019-01-18 00:07:07 -0500

    Add files via upload

---
+++ module2corrected.R
@@ -0,0 +1,3 @@
+assignment2 <- c(16,18,14,22,27,17,19,17,17,22,20,22) 
+myMean <- function(assignment2){return(sum(assignment2)/length(assignment2))} #to find actual mean, it needs to be the sum of the numbers in a set divided by the total number of numbers in the same set
+myMean(assignment2) #running corrected program, though I like my other version better (see blog for details)
